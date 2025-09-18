79136b134254acdd99069f1c9fed27139292ad49
FroelingDev <froelingdev@gmail.com>
2025-09-18 16:28:40 +0100

    delete unused variable

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MapPin, Phone, Mail, Clock, Send, CheckCircle } from 'lucide-react'
 
 export const Route = createFileRoute('/contact')({
